145dc4d427af5c44701259f1343f6f13cd2e7b97
Joost Baaij <joost@spacebabies.nl>
2017-05-23 15:50:03 +0200

    require rails to get specs running

--- spec/scrollytelling/loading_spinner_spec.rb
+++ spec/scrollytelling/loading_spinner_spec.rb
@@ -4,8 +4,4 @@ describe Scrollytelling::LoadingSpinner do
   it 'has a version number' do
     expect(Scrollytelling::LoadingSpinner::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
